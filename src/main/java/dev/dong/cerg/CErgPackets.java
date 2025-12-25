b1b2667949b945b0cca3c00598313590bbb19581
6uopdong <3601778801@qq.com>
2025-12-26 05:58:54 +0800

    feat: 定义按键数据包 Define the key state packet

---
+++ src/main/java/dev/dong/cerg/CErgPackets.java
@@ -0,0 +1,38 @@
+package dev.dong.cerg;
+
+import dev.dong.cerg.content.KeyPressStatePacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkEvent.Context;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
+
+public class CErgPackets {
+    public static final ResourceLocation CHANNEL_NAME = CErg.asResource(CErg.ID);
+    public static final int NETWORK_VERSION = 3;
+    public static final String NETWORK_VERSION_STR = String.valueOf(NETWORK_VERSION);
+    private static SimpleChannel channel;
+
+    public static void registerPackets() {
+        channel = NetworkRegistry.ChannelBuilder.named(CHANNEL_NAME)
+                .serverAcceptedVersions(NETWORK_VERSION_STR::equals)
+                .clientAcceptedVersions(NETWORK_VERSION_STR::equals)
+                .networkProtocolVersion(() -> NETWORK_VERSION_STR)
+                .simpleChannel();
+
+        channel.messageBuilder(KeyPressStatePacket.class, 0, PLAY_TO_SERVER)
+                .encoder(KeyPressStatePacket::write)
+                .decoder(KeyPressStatePacket::new)
+                .consumerNetworkThread((packet, contextSupplier) -> {
+                    Context context = contextSupplier.get();
+                    if (packet.handle(context)) context.setPacketHandled(true);
+                })
+                .add();
+    }
+
+    public static SimpleChannel getChannel() {
+        return channel;
+    }
+
+}
---
+++ src/main/java/dev/dong/cerg/content/KeyPressStatePacket.java
@@ -0,0 +1,52 @@
+package dev.dong.cerg.content;
+
+import com.simibubi.create.foundation.networking.SimplePacketBase;
+import dev.dong.cerg.CErgKeys;
+import dev.dong.cerg.CErg;
+import dev.dong.cerg.CErgPackets;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkEvent.Context;
+
+public class KeyPressStatePacket extends SimplePacketBase {
+    private static final ResourceLocation ID = CErg.asResource("key_state_sync");
+    private final boolean isPressed;
+    private final int keybind;
+
+    public KeyPressStatePacket(FriendlyByteBuf buf) {
+        this.isPressed = buf.readBoolean();
+        this.keybind = buf.readInt();
+    }
+
+    // 自定义构造器
+    public KeyPressStatePacket(boolean isPressed, CErgKeys keybind) {
+        this.isPressed = isPressed;
+        this.keybind = keybind.ordinal();
+    }
+
+    @Override
+    public void write(FriendlyByteBuf buffer) {
+        buffer.writeBoolean(isPressed);
+        buffer.writeInt(keybind);
+    }
+
+    @Override
+    public boolean handle(Context context) {
+        context.enqueueWork(() -> setKeyState(context));
+        return true;
+    }
+
+    private void setKeyState(Context context) {
+        var player = context.getSender();
+        if (player == null) return;
+
+        var key = CErgKeys.indexOf(this.keybind);
+        if (key == null) return;
+
+        PlayerKeyStates.setKeyState(player, key, isPressed);
+    }
+
+    public static void sendToServer(CErgKeys key, boolean pressed) {
+        CErgPackets.getChannel().sendToServer(new KeyPressStatePacket(pressed, key));
+    }
+}
