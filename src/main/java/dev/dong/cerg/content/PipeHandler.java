329db97df2f9cfb3b179cd4799f163eb78c9e59f
6uopdong <3601778801@qq.com>
2026-01-12 20:58:08 +0800

    pref: 开关管道连接时播放动画和音效 Play animations and sound effects when operate pipes

--- src/main/java/dev/dong/cerg/content/PipeHandler.java
+++ src/main/java/dev/dong/cerg/content/PipeHandler.java
@@ -10,6 +10,7 @@ import dev.dong.cerg.util.S2E;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -138,6 +139,12 @@ public class PipeHandler {
         var originState = level.getBlockState(originPos);
         var targetFace = player.isShiftKeyDown() ? clickFace.getOpposite() : clickFace;
         var pre = originState.getValue(PROPERTY_BY_DIRECTION.get(targetFace));
+
+        // 动画和音效
+        player.swing(event.getHand(), true);
+        var soundType = originState.getSoundType(level, originPos, player);
+        level.playSound(null, originPos, soundType.getPlaceSound(), SoundSource.BLOCKS,
+                (soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
 
         level.setBlockAndUpdate(originPos,
                 originState.setValue(PROPERTY_BY_DIRECTION.get(targetFace), !pre));
