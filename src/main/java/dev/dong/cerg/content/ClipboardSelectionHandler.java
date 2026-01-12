3c0a0fcd25a1db2bbc6fcd64df55dae666b810e0
6uopdong <3601778801@qq.com>
2026-01-13 04:03:03 +0800

    feat: 添加剪贴板选区工具类 add clipboard handler

---
+++ src/main/java/dev/dong/cerg/content/ClipboardSelectionHandler.java
@@ -0,0 +1,193 @@
+package dev.dong.cerg.content;
+
+import com.google.common.base.Objects;
+import com.simibubi.create.AllPackets;
+import com.simibubi.create.AllSoundEvents;
+import com.simibubi.create.AllSpecialTextures;
+import com.simibubi.create.CreateClient;
+import com.simibubi.create.content.equipment.clipboard.ClipboardCloneable;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.utility.Components;
+import com.simibubi.create.foundation.utility.Lang;
+import dev.dong.cerg.util.LangUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.HitResult.Type;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import static com.simibubi.create.AllBlocks.CLIPBOARD;
+
+@OnlyIn(Dist.CLIENT)
+public class ClipboardSelectionHandler {
+
+    private static final int SUCCESS = 0x68c586;
+    private static final int HIGHLIGHT = 0xc5b548;
+    private static final int FAIL = 0xff5d6c;
+
+    private final Object clusterOutlineSlot = new Object();
+    private final Object bbOutlineSlot = new Object();
+    private int clusterCooldown;
+
+    private BlockPos firstPos;
+    private BlockPos hoveredPos;
+
+    public void tick() {
+        var mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        BlockPos hovered = null;
+        ItemStack stack = player.getMainHandItem();
+
+        // 若切换了物品则丢弃选区
+        if (!CLIPBOARD.isIn(stack)) {
+            if (firstPos != null)
+                discard();
+            return;
+        }
+
+        // 淡出高亮选区
+        if (clusterCooldown > 0) {
+            if (clusterCooldown == 15)
+                player.displayClientMessage(Components.immutableEmpty(), true);
+            CreateClient.OUTLINER.keep(clusterOutlineSlot);
+            clusterCooldown--;
+        }
+
+        // 获取瞄准方块位置
+        HitResult hitResult = mc.hitResult;
+        if (hitResult != null && hitResult.getType() == Type.BLOCK)
+            hovered = ((BlockHitResult) hitResult).getBlockPos();
+
+        if (hovered == null) {
+            hoveredPos = null;
+            return;
+        }
+
+        // 限制选区范围
+        if (firstPos != null && !firstPos.closerThan(hovered, 24)) {
+            LangUtil.translate("clipboard_selection.too_far")
+                    .color(FAIL)
+                    .sendStatus(player);
+            return;
+        }
+
+        boolean cancel = player.isShiftKeyDown();
+        if (cancel && firstPos == null) return;
+
+        if (Objects.equal(hovered, hoveredPos)) {
+            String key = cancel
+                    ? "clipboard_selection.click_to_discard"
+                    : "clipboard_selection.click_to_confirm";
+            var color = cancel ? HIGHLIGHT : SUCCESS;
+            LangUtil.translate(key)
+                    .color(color)
+                    .sendStatus(player);
+
+            AABB currentSelectionBox = getCurrentSelectionBox();
+            if (currentSelectionBox != null)
+                CreateClient.OUTLINER.showAABB(bbOutlineSlot, currentSelectionBox)
+                        .colored(cancel ? FAIL : HIGHLIGHT)
+                        .withFaceTextures(AllSpecialTextures.CUTOUT_CHECKERED, AllSpecialTextures.CUTOUT_CHECKERED)
+                        .disableLineNormals()
+                        .lineWidth(1 / 16f);
+            return;
+        }
+
+        hoveredPos = hovered;
+    }
+
+    private AABB getCurrentSelectionBox() {
+        return firstPos == null || hoveredPos == null
+                ? null
+                : new AABB(firstPos, hoveredPos).expandTowards(1, 1, 1);
+    }
+
+    public boolean onMouseInput() {
+        var mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        ClientLevel level = mc.level;
+
+        if (!CLIPBOARD.isIn(player.getMainHandItem())) return false;
+        if (!player.mayBuild()) return false;
+
+        if (player.isShiftKeyDown()) {
+            if (firstPos != null) {
+                discard();
+                return true;
+            }
+            return false;
+        }
+
+        if (hoveredPos == null) return false;
+
+        if (firstPos != null) {
+            confirm();
+            return true;
+        }
+
+        firstPos = hoveredPos;
+        LangUtil.translate("clipboard_selection.first_pos")
+                .sendStatus(player);
+        AllSoundEvents.CLIPBOARD_CHECKMARK.playAt(level, firstPos, 0.5F, 0.85F, false);
+        level.playSound(player, firstPos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 0.75f, 1);
+        return true;
+    }
+
+    public void discard() {
+        LocalPlayer player = Minecraft.getInstance().player;
+        firstPos = null;
+        Lang.translate("clipboard_selection.abort")
+                .sendStatus(player);
+        clusterCooldown = 0;
+    }
+
+    public void confirm() {
+        var mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+
+//        AllPackets.getChannel().sendToServer(null);// TODO packet(firstPos, hoveredPos)
+        AllSoundEvents.CLIPBOARD_ERASE.playAt(mc.level, hoveredPos, 0.5F, 0.95F, false);
+        mc.level.playSound(player, hoveredPos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 0.75f, 1);
+
+        AABB currentSelectionBox = getCurrentSelectionBox();
+        if (currentSelectionBox != null)
+            CreateClient.OUTLINER.showAABB(clusterOutlineSlot, currentSelectionBox)
+                    .colored(0xB5F2C6)
+                    .withFaceTextures(AllSpecialTextures.CUTOUT_CHECKERED, AllSpecialTextures.HIGHLIGHT_CHECKERED)
+                    .disableLineNormals()
+                    .lineWidth(1 / 24f);
+
+        discard();
+        LangUtil.translate("clipboard_selection.success")
+                .sendStatus(player);
+        clusterCooldown = 30;
+    }
+
+    public static boolean canPaste(BlockPos pos) {
+        var mc = Minecraft.getInstance();
+        if (!(mc.hitResult instanceof BlockHitResult ray)) return false;
+
+        if (!(mc.level.getBlockEntity(pos) instanceof SmartBlockEntity smartBE)) return false;
+
+        var te = mc.player.getMainHandItem().getTagElement("CopiedValues");
+        if (te == null) return false;
+
+        boolean foundBeh = smartBE.getAllBehaviours().stream()
+                .anyMatch(b -> b instanceof ClipboardCloneable cc && testPaste(cc, te, ray));
+
+        return foundBeh || (smartBE instanceof ClipboardCloneable cc && testPaste(cc, te, ray));
+    }
+
+    private static boolean testPaste(ClipboardCloneable cc, CompoundTag te, BlockHitResult ray) {
+        return cc.readFromClipboard(te.getCompound(cc.getClipboardKey()), Minecraft.getInstance().player, ray.getDirection(), true);
+    }
+}
