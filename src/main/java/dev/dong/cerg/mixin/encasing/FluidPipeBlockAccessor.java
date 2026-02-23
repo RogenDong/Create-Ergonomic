ce56d3f67831058e01c93e9ea5668528262143e7
6uopdong <3601778801@qq.com>
2026-02-24 07:33:09 +0800

    feat(mixin): 调用create管道套壳方法

---
+++ src/main/java/dev/dong/cerg/mixin/encasing/FluidPipeBlockAccessor.java
@@ -0,0 +1,19 @@
+package dev.dong.cerg.mixin.encasing;
+
+import com.simibubi.create.content.fluids.pipes.FluidPipeBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(FluidPipeBlock.class)
+public interface FluidPipeBlockAccessor {
+    @Invoker("useItemOn")
+    ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult);
+}
