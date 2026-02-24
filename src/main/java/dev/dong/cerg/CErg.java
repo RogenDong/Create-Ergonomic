d12fbc8278fcf4ed3a34ba116cba2ae418a5d701
6uopdong <3601778801@qq.com>
2026-02-24 09:25:40 +0800

    fix: 修正事件注册、包引用

--- src/main/java/dev/dong/cerg/CErg.java
+++ src/main/java/dev/dong/cerg/CErg.java
@@ -7,13 +7,11 @@ import dev.dong.cerg.event.PlayerLogged;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(CErg.ID)
@@ -22,18 +20,17 @@ public class CErg {
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final CErgConfig CONFIG = AutoConfig.register(CErgConfig.class, Toml4jConfigSerializer::new).getConfig();
 
-    public CErg() {
-        CONFIG.register();
-        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        var gameEventBus = MinecraftForge.EVENT_BUS;
+    public CErg(ModContainer container, Dist dist) {
+        CONFIG.register(container);
+        var modEventBus = container.getEventBus();
+        var gameEventBus = NeoForge.EVENT_BUS;
         modEventBus(modEventBus);
         gameEventBus(gameEventBus);
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> onClient(modEventBus, gameEventBus));
+        if (dist == Dist.CLIENT) onClient(modEventBus, gameEventBus);
     }
 
     private void modEventBus(IEventBus modEventBus) {
-        modEventBus.addListener(this::addCreative);
-        CErgPackets.registerPackets();
+        CErgPackets.register();
     }
 
     private void gameEventBus(IEventBus gameEventBus) {
@@ -47,12 +44,7 @@ public class CErg {
         gameEventBus.addListener(ClientEvents::onTick);
     }
 
-    // Register the item to a creative tab
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-    }
-
     public static ResourceLocation asResource(String path) {
-        return new ResourceLocation(ID, path);
+        return ResourceLocation.fromNamespaceAndPath(ID, path);
     }
 }
